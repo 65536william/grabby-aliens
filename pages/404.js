aec79a77cc86c6942a14dbcd480e7d77bfcd2c6a
William <admin@williameliot.com>
2021-09-07 22:10:29 +0100

    feat: push to old repo

--- pages/404.js
+++ pages/404.js
@@ -6,8 +6,9 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
   <Layout>
     <SEO
-      title="404 error – Grabby Aliens"
-      description="This page does not exist. Please return to the homepage."
+      title="404 – Grabby Aliens"
+      metaDescription="Page not found."
+      imagePath="x"
     />
     <div className="bg-white lg:p-10 sm:p-5 p-3 shadow lg:rounded-2xl sm:rounded-xl rounded-lg">
       <h1 className="font-serif lg:text-2xl sm:text-xl text-lg">
